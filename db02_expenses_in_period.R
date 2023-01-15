0ac4e8ec710470c0fd45495815a491b3d81ddeb1
Mkranj <mkranjcev6@gmail.com>
2023-01-15 13:34:57 +0100

    Fix date ranges to correspond to input

--- db02_expenses_in_period.R
+++ db02_expenses_in_period.R
@@ -64,8 +64,8 @@ server <- function(input, output, session) {
   
   expenses_individual_data <- reactive({
     filtered_data <- expenses %>% 
-      filter(date_transform > input$date_considered[1],
-             date_transform <= input$date_considered[2] + days(1))
+      filter(date_transform >= input$date_considered[1] - days(1),
+             date_transform <= input$date_considered[2])
     # Filter by input words, if any matches, update data
     matching_filter <- grep(pattern = input$filter_words,
                             x = filtered_data$Note, ignore.case = T)
