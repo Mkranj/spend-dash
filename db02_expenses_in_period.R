2df31fff9dfdf17bbc99d3712a4dde95a52c213f
Mkranj <mkranjcev6@gmail.com>
2023-01-15 11:48:55 +0100

    Change date to 01.01.2022 in new variable

--- db02_expenses_in_period.R
+++ db02_expenses_in_period.R
@@ -11,28 +11,30 @@ finance_data <- select(finance_data, !c(Subcategory, Account.1, Note.1, INR))
 
 expenses <- filter(finance_data, Income.Expense == "Expense")
 
-date_formatted_total <- finance_data$Date %>% strptime("%m/%d/%Y")
+date_transform_total <- finance_data$Date %>% strptime("%m/%d/%Y")
 
-date_formatted_ex <- expenses$Date %>% strptime("%m/%d/%Y")
-expenses$date_formatted <- date_formatted_ex
+date_transform_ex <- expenses$Date %>% strptime("%m/%d/%Y")
+expenses$date_transform <- date_transform_ex
 
 # Not all dates are included - include the ones with no expense or income
 
-all_dates <- seq(min(date_formatted_total),
-                 max(date_formatted_total), "days")
-all_dates <- data.frame(date_formatted = all_dates)
+all_dates <- seq(min(date_transform_total),
+                 max(date_transform_total), "days")
+all_dates <- data.frame(date_transform = all_dates)
 
 expenses <- left_join(
   all_dates,
   expenses)
+
+expenses$date_format <- expenses$date_transform %>% format(format = "%d.%m.%Y")
 
 currency <- unique(finance_data$Currency)[1]
 # FIX! something is in USD, recalculate it.
 
 # App ----
 
-start_date <- min(date_formatted_total)
-end_date <- max(date_formatted_total)
+start_date <- min(date_transform_total)
+end_date <- max(date_transform_total)
 
 ui <- fluidPage(
   theme = shinytheme("flatly"),
@@ -62,8 +64,8 @@ server <- function(input, output, session) {
   
   expenses_individual_data <- reactive({
     filtered_data <- expenses %>% 
-      filter(date_formatted > input$date_considered[1],
-             date_formatted <= input$date_considered[2] + days(1))
+      filter(date_transform > input$date_considered[1],
+             date_transform <= input$date_considered[2] + days(1))
     # Filter by input words, if any matches, update data
     matching_filter <- grep(pattern = input$filter_words,
                             x = filtered_data$Note, ignore.case = T)
@@ -84,7 +86,7 @@ server <- function(input, output, session) {
   })
   
   expenses_daily_data <- reactive({
-    daily_data <- group_by(expenses_individual_data(), date_formatted) %>% 
+    daily_data <- group_by(expenses_individual_data(), date_transform) %>% 
       summarise(expense = sum(Amount, na.rm = T), no_of_expenses = n())
     
     # Days with no expenses still have one row and n of 1
@@ -99,7 +101,7 @@ server <- function(input, output, session) {
     # Function to pass as an argument, to round y-axis
     round_y_axis <- function(y) round(y) 
     
-    plot <- ggplot(plot_data, aes(as.Date(date_formatted), expense)) +
+    plot <- ggplot(plot_data, aes(as.Date(date_transform), expense)) +
       scale_x_date(date_labels = "%m-%Y", date_breaks = "1 months") +
       xlab("Date") + ylab("Amount spent") + theme_minimal() +
       scale_y_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.05)),
@@ -111,7 +113,7 @@ server <- function(input, output, session) {
       plot <- plot + geom_line()
     }
     plot <- plot %>% ggplotly() %>% config(displayModeBar = FALSE)
-    plot$x$data[[1]]$text <- sub("as.Date\\(date_formatted\\)", "Date", plot$x$data[[1]]$text)
+    plot$x$data[[1]]$text <- sub("as.Date\\(date_transform\\)", "Date", plot$x$data[[1]]$text)
     plot$x$data[[1]]$text <- sub("expense", "Expense", plot$x$data[[1]]$text)
     plot
     }
@@ -133,16 +135,16 @@ server <- function(input, output, session) {
   # Change to individual expenses, not daily
   output$table_recent <- renderTable({
     table_data <- expenses_individual_data() %>% filter(!is.na(Amount))
-    table_data$date_formatted <- as.Date(table_data$date_formatted) %>% as.character()
+    table_data$date_transform <- as.Date(table_data$date_transform) %>% as.character()
     if (input$table_sort_type == "recent") {
-      table_data <- arrange(table_data, desc(date_formatted))
+      table_data <- arrange(table_data, desc(date_transform))
       }
     else if (input$table_sort_type == "expensive") {
-      table_data <- arrange(table_data, desc(Amount), desc(date_formatted)) 
+      table_data <- arrange(table_data, desc(Amount), desc(date_transform)) 
       }
     else print("Unknown arrange choice")
     table_data %>%
-      select(date_formatted, Amount, Note) %>% head()
+      select(date_format, Amount, Note, Category) %>% head()
   })
 }
 
