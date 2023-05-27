875380700fdc95c1ef34faf106cef01ebb6432ec
Mkranj <mkranjcev6@gmail.com>
2023-05-27 15:39:16 +0200

    Only necessary columns and USD values

--- data_prep.R
+++ data_prep.R
@@ -1,24 +1,31 @@
-finance_data <- read.csv("expense_data_1.csv")
-finance_data <- select(finance_data, !c(Subcategory, Account.1, Note.1, INR))
+data_filename <- "expense_data_1.csv"
 
+finance_data <- read.csv(data_filename)
+
+# Remove unneeded columns
+finance_data <- select(finance_data, -c(Subcategory,  Account, Account.1, Note.1, INR))
+
+# We're pnly going to be focusing on the expenses
 expenses <- filter(finance_data, Income.Expense == "Expense")
 
-date_transform_total <- finance_data$Date %>% strptime("%m/%d/%Y")
 
-date_transform_ex <- expenses$Date %>% strptime("%m/%d/%Y")
-expenses$date_transform <- date_transform_ex
+# The time is currently written as a timestamp, we'll keep that separate and make
+# a new variable that contains just the date.
+expenses <- rename(expenses,
+                   "Time" = Date) %>%
+  mutate(
+    Date = strptime(Time, "%m/%d/%Y")
+  )
+  
+# Some data is in INR,  other in USD. We'll recalculate everything to USD
 
-# Not all dates are included - include the ones with no expense or income
+# Checked online exchange rate on 27.5.23.
+exchange_rate_INR_USD <- 0.012
 
-all_dates <- seq(min(date_transform_total),
-                 max(date_transform_total), "days")
-all_dates <- data.frame(date_transform = all_dates)
-
-expenses <- left_join(
-  all_dates,
-  expenses)
-
-expenses$date_format <- expenses$date_transform %>% format(format = "%d.%m.%Y")
-
-currency <- unique(finance_data$Currency)[1]
-# FIX! something is in USD, recalculate it.
+expenses <- 
+  mutate(expenses,
+    Amount = ifelse(Currency == "INR", 
+                    Amount * exchange_rate_INR_USD,
+                    Amount),
+    Currency = "USD"
+  )
