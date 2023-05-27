ec77db3cff8c879445ba860d92ff7d4594b3dd7d
Mkranj <mkranjcev6@gmail.com>
2023-05-27 15:47:47 +0200

    App displays table of monthly expenses

--- app.R
+++ app.R
@@ -1,11 +1,28 @@
-library(shiny)
+source("app_setup.R")
+source("data_prep.R")
 
 ui <- fluidPage(
-  
+  dataTableOutput("monthly_data")
 )
 
 server <- function(input, output, session) {
   
+  individual_expenses <- reactive({
+    expenses
+  })
+  
+  expenses_by_month <- reactive({
+    individual_expenses() %>%
+      group_by(Year = year(Date), Month = month(Date)) %>%
+      summarize(NumberOfExpenses = n(),
+                TotalAmount = sum(Amount),
+                AverageExpense = TotalAmount / NumberOfExpenses) 
+  })
+  
+  output$monthly_data <- renderDataTable({
+    datatable(expenses_by_month()) %>%
+      formatRound(columns = c("TotalAmount", "AverageExpense"), digits = 2)
+  })
 }
 
 shinyApp(ui, server)
