c9fa9539bcbc8fbd14d68d6831982c701b930ee3
Mkranj <mkranjcev6@gmail.com>
2023-05-28 09:10:18 +0200

    Create module for picking dates

---
+++ modules/m_dateSelect.R
@@ -0,0 +1,40 @@
+dateSelectUI <- function(id, minDate, maxDate) {
+  ns <- NS(id)
+  tagList(
+    airDatepickerInput(ns("startingDate"), label = "Start date:",
+                       minDate = minDate, maxDate = maxDate,
+                       value = minDate), 
+    airDatepickerInput(ns("endDate"), label = "End date:",
+                       minDate = minDate, maxDate = maxDate,
+                       value = maxDate)
+  )
+}
+
+dateSelectServer <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      
+      date_range <- reactive({
+        list(start = input$startingDate,
+             end = input$endDate)
+      })
+      
+      date_range
+    }
+  )
+}
+
+ui <- fluidPage(
+  dateSelectUI("dates", minDate = "2020-01-01", maxDate = "2021-06-01"),
+  textOutput("selected")
+)
+
+server <- function(input, output, session) {
+  
+  output$selected <- renderText({
+    paste(dateSelectServer("dates")()$start, dateSelectServer("dates")()$end)
+    })
+}
+
+shinyApp(ui, server)
