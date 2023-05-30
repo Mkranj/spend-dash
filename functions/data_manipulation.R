609fc8fc05cce016871f95f2b316ea55d621ec6a
Mkranj <mkranjcev6@gmail.com>
2023-05-30 10:34:02 +0200

    Specify joining by Date
    Faster!

--- functions/data_manipulation.R
+++ functions/data_manipulation.R
@@ -1,6 +1,7 @@
 cover_all_dates_in_period <- function(data) {
   right_join(data,
              data.frame(Date = (min(expenses$Date) : max(expenses$Date)) %>%
-                          as_date())
+                          as_date()),
+             by = "Date"
   ) %>% arrange(Date)
 }
