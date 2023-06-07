e915f8f7d63f085efc8cd233fdf426fdb0db3ecc
Mkranj <mkranjcev6@gmail.com>
2023-06-07 22:16:36 +0200

    Create module for barchart over categories

---
+++ modules/m_categories_barchart.R
@@ -0,0 +1,50 @@
+categories_barchart_UI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    plotlyOutput(ns("categories_bar"))
+    )
+}
+
+categories_barchart_Server <- function(id, individual_expenses) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      stopifnot(is.reactive(individual_expenses))
+      
+      plot_data <- reactive({
+        plot_data <- individual_expenses() %>%
+          group_by(Category) %>% 
+          summarise(Amount = sum(Amount), No_expenses = n(), .groups = "drop")
+      })
+      
+      plot_object <- reactive({
+        req(plot_data)
+        
+        plot_ly(plot_data(), x = ~Category) %>% add_bars(y = ~Amount) %>%
+          layout(
+            xaxis = list(
+              title = list(text = NULL)
+            ),
+            yaxis = list(
+              title = "Expenses",
+              zeroline = F
+            )
+          ) %>% config(displayModeBar = F)
+      })
+      
+      output$categories_bar <- renderPlotly(plot_object())
+      
+    }
+  )
+}
+
+# Example
+# ui <- fluidPage(
+#   categories_barchart_UI("ex")
+# )
+# 
+# server <- function(input, output, session) {
+#   categories_barchart_Server("ex", reactive(expenses))
+# }
+# 
+# shinyApp(ui, server)
