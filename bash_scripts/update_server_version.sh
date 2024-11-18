0d07b06de6e9ede8b5ffd50244a9ede88cb461ff
Mkranj <111303584+Mkranj@users.noreply.github.com>
2024-11-18 14:37:01 +0100

    Add script to move new files to server (#55)
    * Create script to copy all app files
    
    will be modified to access remote server
    
    * move to separate folder
    
    * Copy newest file versions to remote server
    
    * update nginx
    
    * update gitignore

---
+++ bash_scripts/update_server_version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Gather secrets
+source ./envir.sh
+echo "$REMOTE_SERVER"
+
+# Go to project root
+cd ..
+LOCAL_WD=$(pwd)
+
+TARGET_DIR="/SP_app"
+
+# Copy all source files to new folder
+
+#cp {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} "$TARGET_DIR"
+#cp -r {data_files,functions,modules,nginx,scripts,www} "$TARGET_DIR"
+
+pscp -pw $REMOTE_SERVER_PASS {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} $REMOTE_SERVER:$TARGET_DIR
+pscp -pw $REMOTE_SERVER_PASS -r {data_files,functions,modules,nginx,scripts,www} $REMOTE_SERVER:$TARGET_DIR
+pscp -pw $REMOTE_SERVER_PASS bash_scripts/docker_rebuild.sh $REMOTE_SERVER:$TARGET_DIR/bash_scripts/
