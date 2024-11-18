3190cb7de934038e3200ce5efb451f755c9ac11c
Mkranj <mkranjcev6@gmail.com>
2024-11-18 12:48:17 +0100

    Copy newest file versions to remote server

--- bash_scripts/update_server_version.sh
+++ bash_scripts/update_server_version.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# go to root
+# Gather secrets
+source ./envir.sh
+echo "$REMOTE_SERVER"
+
+# Go to project root
 cd ..
 LOCAL_WD=$(pwd)
 
-TARGET_DIR="$LOCAL_WD"/"test_copy/"
+TARGET_DIR="/SP_app"
 
-mkdir "$TARGET_DIR"
 # Copy all source files to new folder
 
-cp {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} "$TARGET_DIR"
-cp -r {data_files,functions,modules,nginx,scripts,www} "$TARGET_DIR"
+#cp {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} "$TARGET_DIR"
+#cp -r {data_files,functions,modules,nginx,scripts,www} "$TARGET_DIR"
+
+pscp -pw $REMOTE_SERVER_PASS {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} $REMOTE_SERVER:$TARGET_DIR
+pscp -pw $REMOTE_SERVER_PASS -r {data_files,functions,modules,nginx,scripts,www} $REMOTE_SERVER:$TARGET_DIR
