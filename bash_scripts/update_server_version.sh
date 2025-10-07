c8aa11f3725e7b54326e067a457f286ef2d930e6
Mkranj <mkranjcev6@gmail.com>
2025-10-07 20:45:01 +0200

    don't copy nginx conf to server

--- bash_scripts/update_server_version.sh
+++ bash_scripts/update_server_version.sh
@@ -12,9 +12,8 @@ TARGET_DIR="/SP_app"
 
 # Copy all source files to new folder
 
-#cp {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} "$TARGET_DIR"
-#cp -r {data_files,functions,modules,nginx,scripts,www} "$TARGET_DIR"
 
 pscp -pw $REMOTE_SERVER_PASS {app.R,compose.yaml,dockerfile,env_variables.R,renv.lock,ui_definition.R} $REMOTE_SERVER:$TARGET_DIR
-pscp -pw $REMOTE_SERVER_PASS -r {data_files,functions,modules,nginx,scripts,www} $REMOTE_SERVER:$TARGET_DIR
+# NGINX is NOT passed over to not mess up newer settings!
+pscp -pw $REMOTE_SERVER_PASS -r {data_files,functions,modules,scripts,www} $REMOTE_SERVER:$TARGET_DIR
 pscp -pw $REMOTE_SERVER_PASS bash_scripts/docker_rebuild.sh $REMOTE_SERVER:$TARGET_DIR/bash_scripts/
